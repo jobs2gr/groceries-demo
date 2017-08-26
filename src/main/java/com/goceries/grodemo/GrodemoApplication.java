3382f162c9a25d48915a4552fad64269b6d5918e
Greeshma <jobs2gr@gmail.com>
2017-08-26 15:39:22 -0500

    initial commit

---
+++ src/main/java/com/goceries/grodemo/GrodemoApplication.java
@@ -0,0 +1,12 @@
+package com.goceries.grodemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrodemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrodemoApplication.class, args);
+	}
+}
